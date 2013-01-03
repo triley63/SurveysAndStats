1bce17822d632487faa666bfc88e77154a09287e
triley63 <rileyta63@yahoo.com>
2013-01-02 22:26:51 -0600

    Updated to run on .NET 3.5 Framework

--- VPS.Client.TeaParty.Survey.Win.UI/mainSurvey.cs
+++ VPS.Client.TeaParty.Survey.Win.UI/mainSurvey.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Configuration;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace VPS.Client.TeaParty.Survey.Win.UI
